8db8e024d8b87f42a0ac284d79af5242d9e658e1
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2018-07-31 09:24:33 +0300

    add traefik and let's encrypt.

--- deploy/bin/deploy-demo.sh
+++ deploy/bin/deploy-demo.sh
@@ -7,4 +7,5 @@ DROPLET_NAME="pvm.demo"
 
 scp "deploy/docker-compose.yml" "root@$DROPLET_NAME:/pvm/docker-compose.yml"
 scp "deploy/.env" "root@$DROPLET_NAME:/pvm/.env"
+scp "deploy/traefik.toml" "root@$DROPLET_NAME:/pvm/traefik.toml"
 ssh "root@$DROPLET_NAME" "cd /pvm && docker stack deploy --compose-file docker-compose.yml pvm_demo"
